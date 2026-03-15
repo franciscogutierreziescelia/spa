44ce1e56bed1428f9c9016bfc3f90d78b08ba529
Francisco Gutiérrez <franciscogutierrez@iescelia.org>
2026-03-15 12:35:37 +0100

    Refactorizando código al paradigma POO

--- js/logica.js
+++ js/logica.js
@@ -1,70 +1,73 @@
-// Create a "close" button and append it to each list item
-const createCloseButtons = () => {
-  const myNodelist = document.getElementsByTagName("LI");
-  for (let i = 0; i < myNodelist.length; i++) {
+class TodoList {
+  constructor() {
+    this.createCloseButtons();
+    this.setupCloseButtonEvents();
+    this.setupListCheckboxEvent();
+    this.setupAddButtonEvent();
+  }
+  // Create a "close" button and append it to each list item
+  createCloseButtons() {
+    const myNodelist = document.getElementsByTagName("LI");
+    for (let i = 0; i < myNodelist.length; i++) {
+      const span = document.createElement("SPAN");
+      const txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      myNodelist[i].appendChild(span);
+    }
+  }
+  // Click on a close button to hide the current list item
+  setupCloseButtonEvents() {
+    const close = document.getElementsByClassName("close");
+    for (let i = 0; i < close.length; i++) {
+      close[i].onclick = (e) => {
+        const div = e.currentTarget.parentElement;
+        div.style.display = "none";
+      };
+    }
+  }
+  // Add a "checked" symbol when clicking on a list item
+  setupListCheckboxEvent() {
+    const list = document.querySelector('ul');
+    list.addEventListener('click', (ev) => {
+      if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+      }
+    }, false);
+  }
+  // Bind Add button event
+  setupAddButtonEvent() {
+    const addBtn = document.querySelector('.addBtn');
+    if (addBtn) {
+      addBtn.addEventListener('click', () => this.newElement());
+    }
+  }
+  // Create a new list item when clicking on the "Add" button
+  newElement() {
+    const li = document.createElement("li");
+    const inputValue = document.getElementById("myInput").value;
+    const t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
     const span = document.createElement("SPAN");
     const txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-  }
-};
-
-// Click on a close button to hide the current list item
-const setupCloseButtonEvents = () => {
-  const close = document.getElementsByClassName("close");
-  for (let i = 0; i < close.length; i++) {
-    close[i].onclick = (e) => {
-      const div = e.currentTarget.parentElement;
-      div.style.display = "none";
-    };
-  }
-};
-
-// Add a "checked" symbol when clicking on a list item
-const setupListCheckboxEvent = () => {
-  const list = document.querySelector('ul');
-  list.addEventListener('click', (ev) => {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
+    li.appendChild(span);
+    // Bind close event for the new item
+    const close = document.getElementsByClassName("close");
+    for (let i = 0; i < close.length; i++) {
+      close[i].onclick = (e) => {
+        const div = e.currentTarget.parentElement;
+        div.style.display = "none";
+      };
     }
-  }, false);
-};
-
-// Create a new list item when clicking on the "Add" button
-const newElement = () => {
-  const li = document.createElement("li");
-  const inputValue = document.getElementById("myInput").value;
-  const t = document.createTextNode(inputValue);
-  li.appendChild(t);
-
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
-
-  const span = document.createElement("SPAN");
-  const txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  const close = document.getElementsByClassName("close");
-  for (let i = 0; i < close.length; i++) {
-    close[i].onclick = (e) => {
-      const div = e.currentTarget.parentElement;
-      div.style.display = "none";
-    };
-  }
-};
-
-// Initialize functionalities
-const initTodoApp = () => {
-  createCloseButtons();
-  setupCloseButtonEvents();
-  setupListCheckboxEvent();
-};
-
-initTodoApp();
+}
+// Initialize the TodoList app
+const todoApp = new TodoList();
